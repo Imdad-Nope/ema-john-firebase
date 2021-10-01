bd794274d15c79ba68a7f620b021892958b79ee6
Imdad-Nope <mdalauddinovi62@gmail.com>
2021-10-01 12:37:22 +0600

    this is first commit

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>This is inventory</h2>
+            <p>this is my inventory where you will find anything.</p>
+        </div>
+    );
+};
+
+export default Inventory;
