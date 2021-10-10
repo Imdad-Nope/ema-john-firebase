11c048849ef2ca01866320da7d28034640469f22
Imdad-Nope <mdalauddinovi62@gmail.com>
2021-10-10 11:37:38 +0600

    ok

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1> Page 404</h1>
+            <h2>Hey there! wrong approach buddy. Search the right keyword.</h2>
+        </div>
+    );
+};
+
+export default NotFound;
